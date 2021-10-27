75777729e4d2ff5c3676d67b96e1c3def6be2189
Jamie Bertram <jamie.bertram@tweag.io>
2021-10-27 08:18:35 -0400

    Add print method

--- src/Servant/PureScript.purs
+++ src/Servant/PureScript.purs
@@ -1,10 +1,14 @@
-module Servant.PureScript where
+module Servant.PureScript (class ToURLPiece, toURLPiece, AjaxError, ErrorDescription(..), printAjaxError) where
 
 import Prelude
-import Affjax (Error, Request, Response)
-import Data.Argonaut.Core (Json, stringify)
-import Data.Argonaut.Decode (JsonDecodeError)
+
+import Affjax (Error, Request, Response, printError)
+import Data.Argonaut.Core (Json, stringify, stringifyWithIndent)
+import Data.Argonaut.Decode (JsonDecodeError, printJsonDecodeError)
 import Data.Argonaut.Encode (class EncodeJson, encodeJson)
+import Data.Array ((:))
+import Data.Newtype (unwrap)
+import Data.String (Pattern(..), joinWith, split)
 
 class ToURLPiece a where
   toURLPiece :: a -> String
@@ -23,3 +27,22 @@ data ErrorDescription
   = UnexpectedHTTPStatus (Response Json)
   | DecodingError JsonDecodeError
   | ConnectingError Error
+
+printAjaxError :: AjaxError -> String
+printAjaxError { description } =
+  joinWith "\n" $
+    "Error making web request:"
+    : ( ("  " <> _)
+          <$> split (Pattern "\n") case description of
+              UnexpectedHTTPStatus response -> printUnexpectedHTTPStatus response
+              DecodingError error -> printJsonDecodeError error
+              ConnectingError error -> printError error
+      )
+
+printUnexpectedHTTPStatus :: Response Json -> String
+printUnexpectedHTTPStatus response =
+  joinWith "\n"
+    [ "Status code: " <> show (unwrap response.status)
+    , "Status text: " <> response.statusText
+    , "Content: " <> stringifyWithIndent 2 response.body
+    ]
