ddf74845e6928132ca959d0284694df809e364b5
Jamie Bertram <jamie.bertram@tweag.io>
2022-01-19 11:04:03 -0500

    Add MonadAjax

--- src/Servant/PureScript.purs
+++ src/Servant/PureScript.purs
@@ -1,14 +1,19 @@
-module Servant.PureScript
-  ( class ToURLPiece
-  , toURLPiece
-  , AjaxError
-  , ErrorDescription(..)
-  , printAjaxError
-  ) where
+module Servant.PureScript where
 
 import Prelude
 
 import Affjax (Error, Request, Response, printError)
+import Affjax as Affjax
+import Control.Monad.Cont (ContT)
+import Control.Monad.Error.Class (throwError)
+import Control.Monad.Except (ExceptT(..), except, mapExceptT, withExceptT)
+import Control.Monad.Identity.Trans (IdentityT)
+import Control.Monad.Maybe.Trans (MaybeT)
+import Control.Monad.RWS (RWST)
+import Control.Monad.Reader (ReaderT)
+import Control.Monad.State (StateT)
+import Control.Monad.Trans.Class (class MonadTrans, lift)
+import Control.Monad.Writer (WriterT)
 import Data.Argonaut
   ( Json
   , JsonDecodeError
@@ -17,15 +22,19 @@ import Data.Argonaut
   , stringifyWithIndent
   )
 import Data.Array ((:))
+import Data.Bifunctor (lmap)
 import Data.BigInt (BigInt)
 import Data.BigInt as BigInt
+import Data.Either (Either)
+import Data.Foldable (class Foldable, foldMap)
 import Data.Int (decimal)
 import Data.Int as Int
 import Data.Newtype (unwrap)
 import Data.Number.Format as Number
-import Data.String (Pattern(..), joinWith, split)
+import Data.String (Pattern(..), drop, joinWith, split)
 import Data.UUID (UUID)
 import Data.UUID as UUID
+import Effect.Aff (Aff)
 
 foreign import encodeURIComponent :: String -> String
 
@@ -50,10 +59,67 @@ instance ToURLPiece BigInt where
 instance ToURLPiece Json where
   toURLPiece = toURLPiece <<< stringify
 
+instance ToURLPiece a => ToURLPiece (Array a) where
+  toURLPiece = toURLPieceFoldable
+
+toURLPieceFoldable :: forall f a. Foldable f => ToURLPiece a => f a -> String
+toURLPieceFoldable = drop 1 <<< foldMap \a -> toURLPiece a <> "/"
+
 type AjaxError =
   { request :: Request Json
   , description :: ErrorDescription
   }
+
+class Monad m <= MonadAjax m where
+  request
+    :: forall a
+     . (Json -> Either JsonDecodeError a)
+    -> Request Json
+    -> ExceptT AjaxError m a
+
+instance MonadAjax Aff where
+  request decode req = do
+    response <-
+      withExceptT (mkError <<< ConnectingError) $ ExceptT $ Affjax.request req
+    let status = unwrap response.status
+    when (status < 200 || status >= 300) do
+      throwError $ mkError $ UnexpectedHTTPStatus response
+    except $ lmap (mkError <<< DecodingError) $ decode response.body
+    where
+    mkError description = { request: req, description }
+
+instance MonadAjax m => MonadAjax (ContT r m) where
+  request = liftRequest
+
+instance MonadAjax m => MonadAjax (ExceptT e m) where
+  request = liftRequest
+
+instance MonadAjax m => MonadAjax (IdentityT m) where
+  request = liftRequest
+
+instance MonadAjax m => MonadAjax (MaybeT m) where
+  request = liftRequest
+
+instance (Monoid w, MonadAjax m) => MonadAjax (RWST r w s m) where
+  request = liftRequest
+
+instance MonadAjax m => MonadAjax (ReaderT r m) where
+  request = liftRequest
+
+instance (Monoid w, MonadAjax m) => MonadAjax (WriterT w m) where
+  request = liftRequest
+
+instance MonadAjax m => MonadAjax (StateT s m) where
+  request = liftRequest
+
+liftRequest
+  :: forall t m a
+   . MonadAjax m
+  => MonadTrans t
+  => (Json -> Either JsonDecodeError a)
+  -> Request Json
+  -> ExceptT AjaxError (t m) a
+liftRequest = map (map (mapExceptT lift)) request
 
 data ErrorDescription
   = UnexpectedHTTPStatus (Response Json)
